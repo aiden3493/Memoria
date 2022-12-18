5e3565de87b8fda2f7e56f77190c8e9b84b4b759
AIDEN <dokyungyu@gmail.com>
2022-12-18 16:24:24 +0900

    remove border of logo box

--- src/components/titleBar/index.tsx
+++ src/components/titleBar/index.tsx
@@ -20,7 +20,7 @@ const TitleBar: React.FC = () => {
       data-tauri-drag-region
       className="flex h-[65px] max-h-[65px] w-full items-center justify-between rounded-lg border-[3px] border-black px-2"
     >
-      <div className="flex h-[45px] w-[45px] cursor-pointer items-center justify-center rounded-md border-[3px] border-black transition-all hover:bg-gray-200">
+      <div className="flex h-[45px] w-[45px] cursor-pointer items-center justify-center rounded-md transition-all hover:bg-gray-200">
         <Image src="/icon.png" width={512} height={512} alt="Memoria" />
       </div>
       <div className="flex items-center justify-center space-x-1">
